c60cff53587ceac6929ebe1c339719b2e70b1ac6
jhonlima97 <jwlimita08.997@gmail.com>
2024-10-13 11:56:08 -0500

    cambios finales

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # apirestfull-drf
 API Rest Full With Django Rest Framework
 
+- Created with Django Rest Framework and Coreapi for documentation
 
 - Generate requirements.txt  
-
 pip freeze > requirements.txt
 
--- apirest/models.py
+++ apirest/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 # Create your models here.
@@ -6,10 +7,16 @@ class Developer(models.Model):
     nickname = models.CharField(max_length=50)
     age      = models.PositiveSmallIntegerField()
     status   = models.BooleanField(default=True)
-    def __str__(self):
-        return self.nickname
     class Meta:
         unique_together=('fullname', 'nickname')
+    def __str__(self):
+        return self.nickname
+    def clean(self):
+        super().clean()
+        if not self.fullname.isalpha():
+            raise ValidationError('The fullname can only contain letters.')
+        if not self.fullname or not self.nickname or not self.age:
+            raise ValidationError('All fields are required.')
 
 class Project(models.Model):
     name        = models.CharField(max_length=100)
@@ -17,8 +24,14 @@ class Project(models.Model):
     start_date  = models.DateField()
     end_date    = models.DateField(null=True, blank=True)
     developer   = models.ForeignKey(Developer, on_delete=models.CASCADE, related_name='projects')
-    
+    class Meta:
+        unique_together=('name',)    
     def __str__(self):
         return self.name
-    class Meta:
-        unique_together=('name',)
+    def clean(self):
+        super().clean()
+        if not self.name.isalpha():
+            raise ValidationError('The name can only contain letters.')
+        if not self.name or not self.description or not self.start_date or not self.developer:
+            raise ValidationError('All fields are required.')
+    
